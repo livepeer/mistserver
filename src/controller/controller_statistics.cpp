dfefbed74d66a69648fff3d206e1607de748533c
Thulinma <jaron@vietors.com>
2023-08-07 13:48:07 +0200

    Added new "source" field to active streams API call

--- src/controller/controller_statistics.cpp
+++ src/controller/controller_statistics.cpp
@@ -766,7 +766,7 @@ void Controller::statSession::update(uint64_t index, Comms::Sessions &statComm){
   } else {
     statComm.setTags("", index);
   }
-  
+
   uint64_t secIncr = prevFirstActive ? (statComm.getNow(index) - prevNow) : 0;
   long long currDown = getDown();
   long long currUp = getUp();
@@ -1523,6 +1523,7 @@ void Controller::fillActive(JSON::Value &req, JSON::Value &rep){
       //fields.append("zerounix");
       fields.append("health");
       fields.append("pid");
+      fields.append("source");
     }
   }
   // collect the data first
@@ -1606,6 +1607,9 @@ void Controller::fillActive(JSON::Value &req, JSON::Value &rep){
         }else if (j->asStringRef() == "health"){
           if (!M || M.getStreamName() != it->first){M.reInit(it->first, false, false);}
           if (M){M.getHealthJSON(F);}
+        }else if (j->asStringRef() == "source"){
+          if (!M || M.getStreamName() != it->first){M.reInit(it->first, false, false);}
+          if (M){F = M.getSource();}
         }else if (j->asStringRef() == "tracks"){
           if (!M || M.getStreamName() != it->first){M.reInit(it->first, false, false);}
           if (M){
