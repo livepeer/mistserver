3a739094ae6d6587c137cb0f349519a9bb64aba9
Thulinma <jaron@vietors.com>
2023-08-16 09:46:29 +0200

    This one will totally fix it for real this time. Fingers crossed :D

--- src/output/output.cpp
+++ src/output/output.cpp
@@ -194,7 +194,6 @@ namespace Mist{
     isInitialized = false;
     wantRequest = false;
     parseData = false;
-    myConn.close();
   }
 
   void Output::initialize(){
@@ -2526,8 +2525,12 @@ namespace Mist{
     if (!statComm){
       statComm.reload(streamName, getConnectedBinHost(), tkn, getStatsName(), reqUrl);
     }
-    if (!statComm){
-      Util::logExitReason(ER_SHM_LOST, "could not connect to session");
+    if (!statComm || statComm.getExit()){
+      if (!statComm){
+        Util::logExitReason(ER_SHM_LOST, "Cannot connect to session");
+      }else{
+        Util::logExitReason(ER_CLEAN_SIGNAL, "Session requested shutdown");
+      }
       onFail("Shutting down since this session is not allowed to view this stream");
       statComm.unload();
       return;
