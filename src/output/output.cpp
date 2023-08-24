80f4553a8dcf9c6fab5f29dc5356854d4a471852
Thulinma <jaron@vietors.com>
2023-08-24 13:00:49 +0200

    When doing initial seek in DTSC output, don't pick the midpoint of each track, but pick a recent-ish keyframe in the main track and fast-forward all tracks independently from there.

--- src/output/output.cpp
+++ src/output/output.cpp
@@ -1198,7 +1198,7 @@ namespace Mist{
     if (!meta){return;}
     meta.removeLimiter();
     uint64_t seekPos = 0;
-    if (meta.getLive() && buffer.getSyncMode()){
+    if (meta.getLive()){
       size_t mainTrack = getMainSelectedTrack();
       if (mainTrack == INVALID_TRACK_ID){return;}
       DTSC::Keys keys(M.keys(mainTrack));
