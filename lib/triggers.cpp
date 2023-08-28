16706915744586198082aad87452e01cb7e1b023
Thulinma <jaron@vietors.com>
2023-08-28 17:24:24 +0200

    Generate a unique trigger-UUID as "X-Trigger-UUID" for every trigger call.

--- lib/triggers.cpp
+++ lib/triggers.cpp
@@ -70,6 +70,7 @@ namespace Triggers{
       if (hrn.size()){
         DL.setHeader("X-Name", hrn);
       }
+      DL.setHeader("X-Trigger-UUID", getenv("MIST_TUUID"));
       if (Util::UUID[0]){DL.setHeader("X-UUID", Util::UUID);}
       DL.setHeader("Content-Type", "text/plain");
       HTTP::URL url(value);
@@ -214,6 +215,23 @@ namespace Triggers{
       WARN_MSG("No triggers for %s: list not ready", type.c_str());
       return false;
     }
+
+    {
+      const char * charset = "0123456789abcdef";
+      std::string uuid = "00000000-0000-4000-D000-000000000000";
+      //Primitive seeding of the prng
+      srand(Util::mix(clock(), time(0), getpid()));
+      for (size_t i = 0; i < uuid.size(); ++i){
+        if (uuid[i] == 'D'){
+          uuid[i] = charset[8+rand()%4];
+          continue;
+        }
+        if (uuid[i] != '0'){continue;}
+        uuid[i] = charset[rand()%16];
+      }
+      setenv("MIST_TUUID", uuid.c_str(), 1);
+    }
+
     size_t splitter = streamName.find_first_of("+ ");
     bool retVal = true;
 
@@ -265,6 +283,8 @@ namespace Triggers{
         }
       }
     }
+
+    unsetenv("MIST_TUUID");
 
     if (dryRun){
       return false;
