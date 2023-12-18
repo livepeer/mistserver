75f67dbd392bfc809a08ec4742609f6b3a980106
Thulinma <jaron@vietors.com>
2023-12-18 02:23:08 +0100

    Silence useless warnings

--- lib/dtsc.cpp
+++ lib/dtsc.cpp
@@ -937,6 +937,7 @@ namespace DTSC{
       sBufMem();
     }else{
       sBufShm(_streamName, DEFAULT_TRACK_COUNT, master, autoBackOff);
+      if (!streamPage){return;}
     }
     streamInit();
     if (isMaster){
@@ -1161,7 +1162,7 @@ namespace DTSC{
     }else{
       streamPage.init(pageName, bufferSize, false, autoBackOff);
       if (!streamPage.mapped){
-        INFO_MSG("Page %s not found", pageName);
+        if (autoBackOff){INFO_MSG("Page %s not found", pageName);}
         return;
       }
       stream = Util::RelAccX(streamPage.mapped, true);
