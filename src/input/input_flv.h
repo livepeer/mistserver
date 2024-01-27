97bdcfe09b85f319a7afe981068703c203c11e96
Thulinma <jaron@vietors.com>
2024-01-27 01:30:29 +0100

    URIReader support for FLV input

--- src/input/input_flv.h
+++ src/input/input_flv.h
@@ -1,12 +1,19 @@
 #include "input.h"
+#include <mist/urireader.h>
 #include <mist/dtsc.h>
 #include <mist/flv_tag.h>
 
 namespace Mist{
-  class InputFLV : public Input{
+  class InputFLV : public Input, public Util::DataCallback{
   public:
     InputFLV(Util::Config *cfg);
     ~InputFLV();
+    bool needsLock();
+    bool needHeader(){return (config->getBool("realtime") || needsLock()) && !readExistingHeader();}
+    virtual void dataCallback(const char *ptr, size_t size);
+    virtual size_t getDataCallbackPos() const;
+    bool openStreamSource(){return true;}
+    virtual bool publishesTracks(){return false;}
 
   protected:
     // Private Functions
@@ -18,7 +25,17 @@ namespace Mist{
     bool keepRunning(bool updateActCtr = true);
     FLV::Tag tmpTag;
     uint64_t lastModTime;
-    FILE *inFile;
+
+    HTTP::URIReader inFile;
+    Util::ResizeablePointer readBuffer;
+    uint64_t readBufferOffset;
+    uint64_t readPos;
+    bool slideWindowTo(size_t seekPos, size_t seekLen = 0);
+
+    virtual size_t streamByteCount(){
+      return totalBytes;
+    }; // For live streams: to update the stats with correct values.
+    size_t totalBytes;
   };
 }// namespace Mist
 
