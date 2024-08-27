cdd9408ffae8e0760a8d01d65b5df33bc0da7bb4
Thulinma <jaron@vietors.com>
2024-08-28 00:06:01 +0200

    Persist Livepeer process cookies as much as possible, but reset them when attempting to switch broadcasters on purpose.

--- src/process/process_livepeer.cpp
+++ src/process/process_livepeer.cpp
@@ -15,6 +15,7 @@
 
 tthread::mutex segMutex;
 tthread::mutex broadcasterMutex;
+std::string cookie;
 
 //Stat related stuff
 JSON::Value pStat;
@@ -461,6 +462,7 @@ void uploadThread(void * num){
       {
         tthread::lock_guard<tthread::mutex> guard(broadcasterMutex);
         target = HTTP::URL(Mist::currBroadAddr+"/live/"+Mist::lpID+"/"+JSON::Value(mySeg.keyNo).asString()+".ts");
+        upper.setHeader("Cookie", cookie);
       }
       upper.dataTimeout = mySeg.segDuration/1000 + 2;
       upper.retryCount = 2;
@@ -483,6 +485,11 @@ void uploadThread(void * num){
           was422 = false;
           prevURL.clear();
           mySeg.fullyWritten = false;
+          {
+            tthread::lock_guard<tthread::mutex> guard(broadcasterMutex);
+            std::string newCookie = upper.getCookie();
+            if (newCookie.size() && newCookie != cookie){cookie = newCookie;}
+          }
           //Wait your turn
           while (myNum != insertTurn && conf.is_active){Util::sleep(100);}
           if (!conf.is_active){return;}//Exit early on shutdown
@@ -532,6 +539,7 @@ void uploadThread(void * num){
       bool switchSuccess = false;
       {
         tthread::lock_guard<tthread::mutex> guard(broadcasterMutex);
+        cookie.clear();
         std::string prevBroadAddr = Mist::currBroadAddr;
         Mist::pickRandomBroadcaster();
         if (!Mist::currBroadAddr.size()){
